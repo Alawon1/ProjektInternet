ca2a5b84d1bd95b9b102595dd5f657607de3a158
Alawon1 <35653724+Alawon1@users.noreply.github.com>
2020-09-24 10:47:28 +0200

    Add files via upload

---
+++ app/index.php
@@ -0,0 +1,12 @@
+<?php
+    if(isset($_GET['miastoA']) and isset($_GET['miastoB'])){
+        
+            include("skrypt.php");      
+    }
+    else{
+        
+        include("form.php");
+    }
+?>
+
+
